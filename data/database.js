be555acd87e9b56a3252d33f8a9341ccbd6d86c4
Khushi Dayal <khushidayal25.94@gmail.com>
2022-08-05 00:56:57 +0800

    Install dotenv package to move DB details in .env file

--- data/database.js
+++ data/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'blog' ,
-    user: 'root',
-    password: 'password'
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool;
