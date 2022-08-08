e886fc237583ebc79652d565f6106b831bf834c7
Khushi Dayal <khushidayal25.94@gmail.com>
2022-08-08 18:08:13 +0800

    Add schema for categories table.

--- sql/install.sql
+++ sql/install.sql
@@ -15,3 +15,11 @@ CREATE TABLE `posts` (
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
