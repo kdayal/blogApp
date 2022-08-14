782b062e2b94916fcf5cc73a9b87710633092f24
Khushi Dayal <khushidayal25.94@gmail.com>
2022-08-14 22:27:57 +0800

    add comments for category routes

--- routes/category.js
+++ routes/category.js
@@ -5,10 +5,14 @@ const categoryController = require('./../controllers/categoryController');
 router.get('/categories', categoryController.getAllCategories);
 router.get('/category/:id', categoryController.getCategory);
 
+// route to display new category form
 router.get('/new-category', categoryController.addCategoryForm);
+// route to handle the new category form submission
 router.post('/category', categoryController.addCategory);
 
+// route to display update category form
 router.get('/category/:id/edit', categoryController.updateCategoryForm);
+// route to handle the update category form submission
 router.post('/category/:id/edit' , categoryController.updateCategory);
 
 router.post('/category/:id/delete', categoryController.deleteCategory);
